d076ed365ec3df09df142819cb5b049a154c89d6
drew <andre@vollrath.ca>
2017-02-04 00:01:25 -0800

    added fontawesome source

--- config/environment.rb
+++ config/environment.rb
@@ -3,17 +3,5 @@ ENV['RACK_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['RACK_ENV'])
 
-##ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/')
-##ActiveRecord::Base.establish_connection('postgres://localhost/')
-
 require_all 'app'
 
-#ActiveRecord::Base.establish_connection(
-#  adapter: 'postgresql',
-#  encoding: 'unicode',
-#  pool: 2,
-#  host: 'localhost',
-#  database: 'fwitter_dev',
-#  username: 'drew',
-#  password: 'drew'
-#)
